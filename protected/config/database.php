b3ba571949a776fb71b0ed8571c6a152f19baee4
Alexey Chuhrai <forneet@gmail.com>
2015-08-05 15:22:59 +0300

    Cheng sqlLight on MySQL And edit Name of Progect $activ gii generator

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,13 +2,14 @@
 
 // This is the database connection configuration.
 return array(
+	/*
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+	*/
+	'connectionString' => 'mysql:host=localhost;dbname=blogedu',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => '123',
 	'charset' => 'utf8',
-	*/
+
 );
