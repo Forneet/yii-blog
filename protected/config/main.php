04f44ee329fb3ebe395a662b3997f6289bf9111c
Alexey Chuhrai <forneet@gmail.com>
2015-08-05 16:18:21 +0300

    generate new file ... gii

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My blog',
+	'name'=>'test YII BLOG',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -27,7 +27,6 @@ return array(
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-
 	),
 
 	// application components
